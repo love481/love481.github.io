c8137ffeac26da18ca31dcf09f3187127524337f
love481 <54012619+love481@users.noreply.github.com>
2025-02-04 11:00:42 +0000

    Deploying to gh-pages from @ love481/love481.github.io@b8e6d4058bc70f44af47411ec694ff389dbfa499 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{
         id: 'social-email',
         title: 'email',
